217d091c7ee57257c1c49ca819724710b0e43ed4
Евгений Коршунов <zamter@yandex.ru>
2023-08-22 20:10:10 +0300

    my

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,10 +62,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
